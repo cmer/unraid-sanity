28e24945e4c2e8bddd5975ea1d4e53a38cb0b244
Carl Mercier <foss@carlmercier.com>
2018-11-22 14:20:51 -0500

    Added tool to help determine changes between the currently supported Unraid version and latest version

---
+++ css_diff.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+require 'open-uri'
+
+GITHUB_USER          = 'limetech'
+GITHUB_REPO          = 'webgui'
+DIFF_TOOL            = 'ksdiff'  # ksdiff or diff
+OPEN_TMP_DIR_ON_EXIT = true
+
+WEBGUI_URL_TEMPLATE  = "https://github.com/#{GITHUB_USER}/#{GITHUB_REPO}/archive/{version}.tar.gz"
+RELEASES_URL         = "https://github.com/#{GITHUB_USER}/#{GITHUB_REPO}/releases"
+RANDOM               = Time.now.to_i
+TMP_DIR              = "/tmp/unraid-dl-#{RANDOM}"
+
+
+supported_version = File.open('./UNRAID_VERSION', &:readline).chomp
+latest_version    = open(RELEASES_URL).read.match(/\/#{GITHUB_USER}\/#{GITHUB_REPO}\/releases\/tag\/([\d\.]*)/i)[1].chomp
+
+puts "Current supported version: #{supported_version}"
+puts "Latest Unraid version: #{latest_version}\n"
+
+if supported_version == latest_version
+  puts "Latest version is already supported. Nothing to diff."
+  exit 1
+end
+
+puts "Downloading & extracting Unraid v#{supported_version}..."
+`mkdir -p #{TMP_DIR}/old`
+`cd #{TMP_DIR}; wget -O old.tar.gz #{WEBGUI_URL_TEMPLATE.sub('{version}', supported_version)} > /dev/null 2>&1`
+`tar xfz #{TMP_DIR}/old.tar.gz --strip-components=2 -C #{TMP_DIR}/old > /dev/null`
+
+puts "Downloading & extracting Unraid v#{latest_version}..."
+`mkdir -p #{TMP_DIR}/new`
+`cd #{TMP_DIR}; wget -O new.tar.gz #{WEBGUI_URL_TEMPLATE.sub('{version}', latest_version)} > /dev/null 2>&1`
+`tar xfz #{TMP_DIR}/new.tar.gz --strip-components=2 -C #{TMP_DIR}/new > /dev/null`
+
+puts "Removing all non-CSS files..."
+`cd #{TMP_DIR}; find . -type f ! -name '*.css' -delete`
+
+if DIFF_TOOL == 'diff'
+  `diff -r -E -b -w -B #{TMP_DIR}/old #{TMP_DIR}/new`
+elsif DIFF_TOOL == 'ksdiff'
+  `ksdiff #{TMP_DIR}/old #{TMP_DIR}/new`
+else
+  puts "Invalid diff tool: #{DIFF_TOOL}. Exiting."
+  exit 1
+end
+
+`open #{TMP_DIR}` if OPEN_TMP_DIR_ON_EXIT
